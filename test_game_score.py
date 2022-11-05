c6bd057a2c431fe495f03b844d3ac63b4666d846
andrey <rodandr13@gmail.com>
2022-11-05 17:55:31 +0300

    feat: add tests

--- test_game_score.py
+++ test_game_score.py
@@ -4,18 +4,42 @@ import random
 from game_score import get_score, generate_game, TIMESTAMPS_COUNT, OFFSET_MAX_STEP
 
 
-class TestGetScore(unittest.TestCase):
+class TestGameScore(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
-        cls.game_stamps = generate_game()
-        cls.offset = random.randint(0, TIMESTAMPS_COUNT * OFFSET_MAX_STEP)
-        cls.get_score = get_score(TestGetScore.game_stamps, TestGetScore.offset)
+        cls.valid_game_stamps = [
+             {'offset': 0, 'score': {'away': 0, 'home': 0}},
+             {'offset': 2, 'score': {'away': 1, 'home': 0}},
+             {'offset': 4, 'score': {'away': 2, 'home': 1}},
+             {'offset': 7, 'score': {'away': 2, 'home': 1}}
+        ]
+        cls.invalid_game_stamps = [
+             {'offset': 0, 'score': {'away': 0, 'home': 0}},
+             {'offset': '2', 'score': {'away': 1, 'home': 0}},
+             {'offset': 4, 'score': {'away': 2, 'home': 1}}
+        ]
+        cls.existing_offset = 2
+        cls.missing_offset = 1
+
+    def test_valid_arguments(self):
+        call = get_score(self.valid_game_stamps, self.existing_offset)
+        result = (0, 1)
+        self.assertEqual(call, result)
 
     def test_invalid_argument_type_offset(self):
-        self.assertRaises()
-        print(self.get_score)
+        with self.assertRaises(TypeError):
+            get_score(self.invalid_game_stamps, self.existing_offset)
+
+    def test_invalid_argument_value_offset(self):
+        max_offset = TIMESTAMPS_COUNT * OFFSET_MAX_STEP + 1
+        for i in (-1, max_offset):
+            with self.subTest(value=i):
+                self.assertRaises(ValueError, get_score, self.valid_game_stamps, i)
+
+    def test_missing_offset_in_game_stamps(self):
+        call = get_score(self.valid_game_stamps, self.missing_offset)
+        self.assertIsNone(call, "Функция должна возвращать None при отсутствии временной отсечки offset")
 
 
 if __name__ == '__main__':
